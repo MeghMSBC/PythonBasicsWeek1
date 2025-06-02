170f710a8efb87cf0c1dce58be7a04b4b27f64c9
Megh Chakravarty <Megh.Chakravarty@MSBC-MAINFRAME.LCL>
2025-06-02 19:15:19 +0530

    Final Version

--- pandasBasics.py
+++ pandasBasics.py
@@ -1,7 +1,7 @@
 import pandas as pd
 df = pd.DataFrame({
     "Name":["Aman","Biju","Charlie","Diya"],
-    "Age" : [25,30,35,None],
+    "Age" : [25,30,35,40],
 },index=['a','b','c','d'])
 
 #using df.loc to access rows directly use df["Tag"] to access full columns
@@ -101,7 +101,53 @@ print(result)
 result = df.melt(id_vars=['Name'],value_vars=["Age"])
 print(result)
 
-#sorting
+#sorting 
+# stores the entire data frame again in the said sorted order
 
 result = groupdf.sort_values('weight',ascending=True)
 print(result)
+
+#ranking if there are same values then takes averages ex:3rd and 4th are same then will mark both as 3+4 whole /2 (spearmans rank corelation types)
+result= df['Rank'] = df['Age'].rank(ascending=True)
+print(result)
+
+#date time 
+datedf = pd.DataFrame({
+    'date': ['2023-01-01', '2023-01-02', '2023-01-03'],
+    'value': [100, 110, 105]
+})
+
+datedf['date'] = pd.to_datetime(datedf['date'])  # Converts string to datetime
+print(datedf['date'])
+
+#resampling  if ur upsampling then u can use forward fill backward fill or extrapolating etc to fil in the values
+#for downsampling we can take the min max or mean or sum etc to fill the values 
+date_range = pd.date_range(start='2023-01-01', end='2023-01-10', freq='D') #time series data frame
+
+df = pd.DataFrame({
+    'date': date_range,
+    'sales': [100, 120, 130, 90, 110, 150, 160, 170, 180, 190]
+})
+
+df.set_index('date', inplace=True)
+
+print("Original Daily Data:")
+print(df)
+
+monthly_avg = df.resample('H').mean()
+
+print("\nMonthly Resampled (Mean Sales):")
+print(monthly_avg)
+
+
+#vectorization faster than running loops
+groupdf['weight'] = groupdf['weight']*2 
+print(groupdf)
+
+
+#categorical data  uses less memory as there are limited tags so the data is stored
+#order of categories can also be mentioned
+groupdf['animal'] = groupdf['animal'].astype('category')
+
+print(groupdf['animal'])
+print(groupdf['animal'].cat.categories)
