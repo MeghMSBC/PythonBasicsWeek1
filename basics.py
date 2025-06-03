40b25072390ddb88e0ea8028612a0fa5225b58f8
Megh Chakravarty <Megh.Chakravarty@MSBC-MAINFRAME.LCL>
2025-06-03 19:26:49 +0530

    daytwo

---
+++ basics.py
@@ -0,0 +1,35 @@
+# list=[]
+# for i in range(3):
+#    num= input("Enter "+str(i)+" Number: ")
+#    list.append(int(num))
+
+# i=0
+# j=len(list)-1
+# flag=1
+# while i<j:
+#     if list[i]!=list[j]:
+#         flag=0
+#         break
+#     else:
+#         i+=1
+#         j-=1
+# if(flag!=0):
+#     print("Is a palindrome")
+# else:
+#     print("Is not a palindrome")
+
+# rev = list.copy()
+# rev.reverse()
+# print(rev)
+# if(list==rev):
+#    print("Is a palindrome")
+# else:
+#    print("Is not a palindrome")
+
+
+#dictionary
+dict = {
+   "table":["a piece of furniture","list of facts & figures"],
+   "cat":["a small animal"]
+}
+print(dict)
